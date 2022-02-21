c00850ec42f8e71143217166d6733dcb1da1a025
Kamil Musialik <kamilmusialik22@gmail.com>
2022-02-21 12:26:06 +0100

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 
+var argButtonName, buttonPaper, buttonRock, buttonScissors, buttonTest;
+function buttonClicked(argButtonName) {
+  clearMessages();
+  console.log(`${argButtonName} został kliknięty`);
+}
+
+buttonTest = document.getElementById('button-test');
+buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
+buttonRock = document.getElementById('button-rock');
+buttonRock.addEventListener('click', function(){ buttonClicked('Guzik KAMIEŃ'); });
+buttonPaper = document.getElementById('button-paper');
+buttonPaper.addEventListener('click', function(){ buttonClicked('Guzik PAPIER'); });
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){ buttonClicked('Guzik NOŻYCE'); });
+
 var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
 function getMoveName(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
@@ -30,12 +45,21 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
 }
 
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+playerMove = argButtonName;
 console.log('wybór ruchu gracza to: ' + playerInput);
-playerMove = getMoveName(playerInput);
 console.log('ruch gracza to: ' + playerMove);
 randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
 displayResult(playerMove, computerMove);
+
+buttonTest.addEventListener('click', function(){ 
+  buttonClicked('Test button'); });
+buttonRock.addEventListener('click', function(){ 
+  buttonClicked('Rock button'); });
+buttonPaper.addEventListener('click', function(){ 
+  buttonClicked('Paper button'); });
+buttonScissors = document.getElementById('button-scissors');
+buttonScissors.addEventListener('click', function(){ 
+  buttonClicked('Scissors button'); });
